fbae22372c463e3beada77b6616df89bd5a119a2
Zlatan Stadler <zlat0179@edu.easj.dk>
2021-04-21 21:01:35 +0200

    commit

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,36 @@
-import dotenv from 'dotenv'; 
+import dotenv from 'dotenv';
 import { Client } from 'discord.js';
 dotenv.config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 
 const client = new Client();
 
 
-//client.login("ODM0NDUzMjkxNTA0ODk0MDI4.YIBHMA.eehvJhIYBMt_p__WVixP22tHyow");
-client.login(DISCORD_TOKEN);
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.login(DISCORD_TOKEN);
+client.on('ready', () => 
+{
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg =>
+{
+  if(msg.author.id == "834453291504894028") return;
+  console.log(DISCORD_CHANNEL_ID);
+  if (msg.channel.id == DISCORD_CHANNEL_ID)
+  {
+    if (msg.content === 'ping') 
+    {
+      msg.reply('Pong!!');
+    }
+    if (msg.content == 'er jeg en bot'){
+      if (!msg.author.bot){
+        msg.reply("Du er ikke en bot")
+      }
+      else{
+        msg.reply('du er en bot')
+      }
+    }
   }
 });
